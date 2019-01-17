9209a3bf30996a609aa0d4476be9977ebc927fcb
Luis Adame <mrluissan@gmail.com>
2019-01-17 02:41:51 +0100

    Add landing view

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('content')
+<main class="container d-flex justify-content-center align-items-center flex-grow-1">
+    <h2>Welcome to Zero Waste</h2>
+</main>
+@endsection
