204dd4fd15ac48c2f863b3d30ef8d72e92cc0d9c
Luis Adame <mrluissan@gmail.com>
2019-01-23 17:54:11 +0100

    Add migrations and seeders

---
+++ database/migrations/2019_01_21_160121_create_logins_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLoginsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logins', function (Blueprint $table) {
+            $table->unsignedInteger('user_id');
+            $table->boolean('successful');
+            $table->ipAddress('ip');
+            $table->timestamp('happened_at')->default(now());
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('logins');
+    }
+}
