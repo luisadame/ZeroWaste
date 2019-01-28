6353e8413ca00bd3a1af0757bb21a2adf621a0c1
Luis Adame <mrluissan@gmail.com>
2019-01-28 16:51:55 +0100

    Add about and contact views

---
+++ resources/views/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('content')
+<main class="container d-flex justify-content-center align-items-center flex-grow-1">
+    <h2>About</h2>
+</main>
+@endsection
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('content')
+<main class="container d-flex justify-content-center align-items-center flex-grow-1">
+    <h2>Contact</h2>
+</main>
+@endsection
