2ca1bf755d4edba0754376adf6861ef4b10f8984
Luis Adame <mrluissan@gmail.com>
2019-01-29 21:19:46 +0100

    Add models

--- app/User.php
+++ app/User.php
@@ -27,4 +27,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function inventories()
+    {
+        return $this->hasMany('App\Inventory');
+    }
 }
