8c0b741c670971c1c1990c8fd03513a4470a33e9
Luis Adame <mrluissan@gmail.com>
2019-02-12 19:11:26 +0100

    Change food seeder

--- database/seeds/FoodSeeder.php
+++ database/seeds/FoodSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\FoodType;
 
 class FoodSeeder extends Seeder
 {
@@ -11,6 +12,12 @@ class FoodSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Food::class, 10)->create();
+        factory(App\Food::class, 10)
+            ->create()
+            ->each(function ($food) {
+                $food->types()->attach(
+                    FoodType::inRandomOrder()->limit(rand(1, 3))->get()
+                );
+            });
     }
 }
