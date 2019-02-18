93f6eb95cd7daa05bbff376a813f7911d48aaeb9
Luis Adame <mrluissan@gmail.com>
2019-02-18 18:06:41 +0100

    Render all food items with card component

--- resources/views/private/inventory/show.blade.php
+++ resources/views/private/inventory/show.blade.php
@@ -3,7 +3,7 @@
     <div class="container mt-3">
         {{ Breadcrumbs::render('inventory', $inventory) }}
         <h2>@flang('pages.inventory', true): {{ $inventory->name }}</h2>
-        <div class="d-flex options">
+        <div class="d-flex options my-2">
             <a href="{{ route('inventories.edit', $inventory->id) }}" class="btn btn-warning" tabindex="-1" role="button">
                 Edit
             </a>
@@ -13,8 +13,20 @@
                 <button class="btn btn-link">Delete</button>
             </form>
         </div>
+        <h3>Your food items</h3>
         <div class="container-fluid grid mt-3 p-0">
-            <h2>ble</h2>
+            @forelse($foodCollection as $food)
+                @component(
+                    'private.components.card',
+                    ['classes' => [$food->expirationProximity()]]
+                )
+                    @slot('route', '')
+                    @slot('title', $food->name)
+                    @slot('text', $food->expirationString())
+                @endcomponent
+            @empty
+                <p>__('empty.no_food_yet')</p>
+            @endforelse
         </div>
     </div>
 @endsection
