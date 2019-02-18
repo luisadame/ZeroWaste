1b1bff241db58e08a7e2b5fdf5989d92bb37a059
Luis Adame <mrluissan@gmail.com>
2019-02-18 18:07:44 +0100

    Add expiration date methods

--- app/Food.php
+++ app/Food.php
@@ -8,11 +8,57 @@ class Food extends Model
 {
     protected $table = 'food';
 
+    /**
+     * Attributes that should be mutated to dates
+     *
+     * @var array
+     */
+    protected $dates = [
+        'expiration_date'
+    ];
+
+    /**
+     * Get a lozalized string formatted with the expiration date
+     */
+    public function expirationString()
+    {
+        $tense = $this->expiration_date->isPast() ? trans('dates.expired_on') : trans('dates.expires_on');
+        return $tense . ' ' . $this->expiration_date->toDayDateTimeString();
+    }
+
+    /**
+     * Get a css class to visually inform the user how close is
+     * an item to be expired
+     *
+     * @return void
+     */
+    public function expirationProximity()
+    {
+        $daysUntilExpiration = now()->diffInDays($this->expiration_date, false);
+        if ($daysUntilExpiration < 10) {
+            if ($daysUntilExpiration < 0) {
+                return 'card--danger';
+            }
+            return 'card--warning';
+        }
+        return '';
+    }
+
+    /**
+     * Relationship with its type
+     *
+     * @return void
+     */
     public function types()
     {
         return $this->belongsToMany('App\FoodType', 'food_food_type');
     }
 
+    /**
+     * Relationship with the inventory
+     *
+     * @return void
+     */
     public function inventories()
     {
         return $this->belongsToMany('App\Inventory', 'inventory_food');
