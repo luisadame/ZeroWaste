5236de724b445243c51fc3669ff0ad4ae032266f
Luis Adame <mrluissan@gmail.com>
2019-02-18 18:04:51 +0100

    Add random expiration date for food

--- database/factories/FoodFactory.php
+++ database/factories/FoodFactory.php
@@ -5,6 +5,6 @@ use Faker\Generator as Faker;
 $factory->define(App\Food::class, function (Faker $faker) {
     return [
         'name' => $faker->word,
-        'expiration_date' => $faker->date
+        'expiration_date' => now()->addDays(rand(-10, 20))
     ];
 });
