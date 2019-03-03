f96c122909f02414eade496b372e36b06666b7aa
Luis Adame <mrluissan@gmail.com>
2019-03-03 04:16:24 +0100

    Create food component

---
+++ resources/views/private/components/food.blade.php
@@ -0,0 +1,31 @@
+<a href="{{ $route }}" class="
+    text-decoration-none
+    text-body
+">
+    <div class="
+        card
+        inventory
+        @isset($classes)
+            @foreach($classes as $class)
+                {{$class}}
+            @endforeach
+        @endisset
+    ">
+        <div class="card-options">
+            <form action="{{ route('food.destroy', $food) }}" method="POST">
+                @csrf
+                @method('DELETE')
+                <button class="btn btn-danger btn-sm" type="submit">Delete</button>
+            </form>
+        </div>
+        <div class="card-body">
+            @isset($img)
+            {!! $img !!}
+            @endisset
+            <h2 class="card-title">{{ $title }}</h2>
+            @isset($text)
+            <p class="card-text">{{ $text }}</p>
+            @endisset
+        </div>
+    </div>
+</a>
