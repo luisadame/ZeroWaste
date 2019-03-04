bc0b08a3289b8363f12a6d489a8c59e966aa4a2b
Luis Adame <mrluissan@gmail.com>
2019-03-04 14:08:47 +0100

    Change image's id type to uuid on database

--- database/migrations/2019_01_29_190322_create_images_table.php
+++ database/migrations/2019_01_29_190322_create_images_table.php
@@ -14,7 +14,7 @@ class CreateImagesTable extends Migration
     public function up()
     {
         Schema::create('images', function (Blueprint $table) {
-            $table->increments('id');
+            $table->uuid('id')->primary()->unique();
             $table->string('url');
             $table->unsignedInteger('imageable_id');
             $table->string('imageable_type');
