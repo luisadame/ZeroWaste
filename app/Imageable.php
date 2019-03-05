99d037fedee998d10a8953d80f6f2103d97b7a58
Luis Adame <mrluissan@gmail.com>
2019-03-06 00:14:05 +0100

    Reformat Imageable [style]

--- app/Imageable.php
+++ app/Imageable.php
@@ -10,23 +10,27 @@ abstract class Imageable extends Model
 {
     public function saveImages($images)
     {
-        $images = array_map(function ($serverId) {
-            $image = new Image();
-            $tempPath = $image->getPathFromServerId($serverId);
-            $fileName = sprintf('%s.%s', File::name($tempPath), File::extension($tempPath));
-            $source = Storage::disk('temporary')
-                ->getDriver()
-                ->getAdapter()
-                ->applyPathPrefix($fileName);
-            $dest = Storage::disk('images')
-                ->getDriver()
-                ->getAdapter()
-                ->applyPathPrefix($fileName);
-            if (File::move($source, $dest)) {
-                $image->path = $fileName;
-                return $image;
-            }
-        }, $images);
+        $images = array_map(
+            function ($serverId) {
+                $image = new Image();
+                $tempPath = $image->getPathFromServerId($serverId);
+                $fileName = sprintf('%s.%s', File::name($tempPath), File::extension($tempPath));
+                $source = Storage::disk('temporary')
+                    ->getDriver()
+                    ->getAdapter()
+                    ->applyPathPrefix($fileName);
+                $dest = Storage::disk('images')
+                    ->getDriver()
+                    ->getAdapter()
+                    ->applyPathPrefix($fileName);
+
+                if (File::move($source, $dest)) {
+                    $image->path = $fileName;
+                    return $image;
+                }
+            },
+            $images
+        );
 
         $this->images()->saveMany($images);
     }
