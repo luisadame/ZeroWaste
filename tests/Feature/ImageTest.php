123f92b253b538d56363b7f7bce0c52c4883f171
Luis Adame <mrluissan@gmail.com>
2019-03-07 03:40:51 +0100

    Test a user can upload images and remove them

--- tests/Feature/ImageTest.php
+++ tests/Feature/ImageTest.php
@@ -8,18 +8,22 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Http\Response;
 use App\User;
 use Illuminate\Http\UploadedFile;
+use App\Image;
+use Illuminate\Support\Facades\Storage;
 
 class ImageTest extends TestCase
 {
     use RefreshDatabase;
 
     private $user;
+    private $image;
 
     public function setUp()
     {
         parent::setUp();
         $this->seed(\AdminUserSeeder::class);
         $this->user = User::where('email', 'admin@example.com')->first();
+        $this->image = new Image();
     }
 
     private function images()
@@ -27,12 +31,31 @@ class ImageTest extends TestCase
         $images = [];
 
         for ($i = 0; $i < rand(1, 9); $i++) {
-            $images[] = UploadedFile::fake()
+            $images[$i] = UploadedFile::fake()
                 ->image("delicious_omelette{$i}.jpg", 640, 480)
                 ->size(200);
         }
 
         return $images;
+    }
+
+    public function ajaxWithContent($method, $uri, $content = '')
+    {
+        $headers = [
+            'CONTENT_LENGTH' => mb_strlen($content, '8bit'),
+            'CONTENT_TYPE' => 'application/json',
+            'Accept' => 'application/json',
+        ];
+
+        return $this->call(
+            $method,
+            $uri,
+            [],
+            [],
+            [],
+            $this->transformHeadersToServerVars($headers),
+            $content
+        );
     }
 
     /** @test */
@@ -43,5 +66,70 @@ class ImageTest extends TestCase
 
         $this->postJson(route('images.store'), compact('images'))
             ->assertStatus(Response::HTTP_UNAUTHORIZED);
+    }
+
+    /** @test */
+    public function authenticated_user_can_upload_images()
+    {
+        $this->disableExceptionHandling();
+        Storage::fake('temporary');
+
+        $this->actingAs($this->user);
+
+        // create the images
+        $images = $this->images();
+
+        // we'll save server id in here
+        $serverIds = [];
+
+        // we use a library that sends images one by one
+        // lets test the exact same behaviour
+        foreach ($images as $image) {
+            $response = $this->postJson(route('images.store'), ['images' => [$image]]);
+
+            $response->assertOk();
+            $serverIds[] = $response->getContent();
+        }
+
+        // decrypt all of these image server ids
+        $paths = array_map(
+            function ($serverId) {
+                return $this->image->getPathFromServerId($serverId);
+            },
+            $serverIds
+        );
+
+        // assert these pats exist
+        foreach ($paths as $path) {
+            Storage::disk('temporary')->assertExists($path);
+        }
+    }
+
+    /** @test */
+    public function unauthenticated_user_cant_remove_temporal_images()
+    {
+        $this->ajaxWithContent('DELETE', route('images.destroy'), '12345')
+            ->assertStatus(Response::HTTP_UNAUTHORIZED);
+    }
+
+    /** @test */
+    public function authenticated_user_can_remove_temporal_images()
+    {
+        Storage::fake('temporary');
+
+        $this->actingAs($this->user);
+
+        // save images and encrypt the names as we would do on controller
+        $serverIds = [];
+        foreach ($this->images() as $fakeImage) {
+            $serverIds[] = $this->image->getServerIdFromPath($fakeImage->store('', 'temporary'));
+        }
+
+        // assert these pats exist
+        foreach ($serverIds as $serverId) {
+            $this->ajaxWithContent('DELETE', route('images.destroy'), $serverId)
+                ->assertOk();
+            Storage::disk('temporary')->assertMissing($this->image->filename($serverId));
+        }
     }
 }
