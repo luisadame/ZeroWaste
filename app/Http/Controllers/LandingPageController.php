4112b71636e15f21f2c7d59250ef67eff82928a7
Luis Adame <mrluissan@gmail.com>
2019-03-10 17:51:02 +0100

    Add guest middleware on landing page

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class LandingPageController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
     public function __invoke()
     {
         return view('landing');
